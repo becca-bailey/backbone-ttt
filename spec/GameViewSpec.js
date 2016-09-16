11d4b968683cf713f10d359b87866ab1c9d459d5
Becca Nelson <beccanelson88@gmail.com>
2016-09-16 10:49:39 -0500

    Test GameView with a MockCompiler

--- spec/GameViewSpec.js
+++ spec/GameViewSpec.js
@@ -1,11 +1,11 @@
 var GameView = require('../app/views/GameView');
 var HumanVsComputerGame = require('../app/models/HumanVsComputerGame');
 var $ = require('jquery');
-var HandlebarsCompiler = require('../app/http/HandlebarsCompiler');
+var MockCompiler = require('./mocks/MockCompiler');
 
 describe("GameView", function() {
   beforeEach(function() {
-    compiler = new HandlebarsCompiler()
+    compiler = new MockCompiler()
     game = new HumanVsComputerGame({compiler: compiler});
     gameView = new GameView({model: game});
     jasmine.getFixtures().fixturesPath = '../partials';
@@ -28,27 +28,27 @@ describe("GameView", function() {
     expect($("#status")).toBeDefined();
   });
 
-//  it("has a nested boardView", function() {
-//    expect(gameView.boardView).toBeDefined();
-//  });
-//
-//  it("has a nested statusView", function() {
-//    expect(gameView.statusView).toBeDefined();
-//  });
-//
-//  describe("resetGame", function() {
-//    beforeEach(function() {
-//      spyOn(gameView.model, "resetAttributes");
-//      spyOn(gameView.boardView, "enableAllSpots");
-//      gameView.resetGame();
-//    });
-//
-//    it("resets all attributes in the model", function() {
-//      expect(gameView.model.resetAttributes).toHaveBeenCalled();
-//    });
-//
-//    it("enables all spots", function() {
-//      expect(gameView.boardView.enableAllSpots).toHaveBeenCalled();
-//    });
-//  });
+  it("has a nested boardView", function() {
+    expect(gameView.boardView).toBeDefined();
+  });
+
+  it("has a nested statusView", function() {
+    expect(gameView.statusView).toBeDefined();
+  });
+
+  describe("resetGame", function() {
+    beforeEach(function() {
+      spyOn(gameView.model, "resetAttributes");
+      spyOn(gameView.boardView, "enableAllSpots");
+      gameView.resetGame();
+    });
+
+    it("resets all attributes in the model", function() {
+      expect(gameView.model.resetAttributes).toHaveBeenCalled();
+    });
+
+    it("enables all spots", function() {
+      expect(gameView.boardView.enableAllSpots).toHaveBeenCalled();
+    });
+  });
 }); 
